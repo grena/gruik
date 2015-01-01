a42b8c7599feefb654be8f9a82cd969e37bfe227
Adrien Pétremann <petremann.adrien@gmail.com>
2015-01-01 19:15:22 +0100

    js(files): Delete unused JS files & use packages instead

--- app/views/front/login.blade.php
+++ app/views/front/login.blade.php
@@ -6,7 +6,7 @@
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <link rel="Shortcut Icon" type="image/png" href="/img/gruik-black.png" />
 
-        <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="/vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <link href="/css/AdminLTE.css" rel="stylesheet" type="text/css" />
 
@@ -50,7 +50,7 @@
 
         <script src="/vendor/jquery/jquery.min.js"></script>
         <script src="/vendor/checklist-model/checklist-model.js"></script>
-        <script src="/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="/vendor/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
         <script src="/vendor/smoke.js/smoke.min.js" type="text/javascript"></script>
         <script src="/js/angular/main.js" type="text/javascript"></script>
 
