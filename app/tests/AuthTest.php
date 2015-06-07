69763ed6cfa938b31c264b5f78358dc95d86574c
Nounours Heureux <schtroumps31@gmail.com>
2015-06-07 20:05:20 +0200

    Added Register tests

--- app/tests/AuthTest.php
+++ app/tests/AuthTest.php
@@ -1,5 +1,4 @@
 <?php
-
 class AuthTest extends TestCase {
 
     public function testLoginOk()
@@ -11,25 +10,62 @@ class AuthTest extends TestCase {
     public function testLoginEmailRequired()
     {
         $response = $this->action('POST','AuthController@login',array('password' => 'bouyalol'));
-        $this->assertEquals(json_decode($response->getContent())->flash,"Login field is required.");
+        $this->assertEquals("Login field is required.", json_decode($response->getContent())->flash);
     }
 
     public function testLoginPasswordRequired()
     {
         $response = $this->action('POST','AuthController@login',array('email' => 'e@mail.com'));
-        $this->assertEquals(json_decode($response->getContent())->flash,"Password field is required.");
+        $this->assertEquals("Password field is required.", json_decode($response->getContent())->flash);
     }
 
     public function testLoginBadPassword()
     {
         $response = $this->action('POST','AuthController@login',array('email' => 'test@example.com','password' => 'badpassword'));
-        $this->assertEquals(json_decode($response->getContent())->flash,"Wrong password, try again.");
+        $this->assertEquals("Wrong password, try again.", json_decode($response->getContent())->flash);
     }
 
     public function testLoginUserNotFound()
     {
         $response = $this->action('POST','AuthController@login',array('email' => 'not@found.com','password' => 'pass'));
         $this->assertEquals(json_decode($response->getContent())->flash,"User was not found.");
+        $this->assertEquals("User was not found.", json_decode($response->getContent())->flash);
+    }
+
+    public function testRegisterEmailRequired()
+    {
+        $response = $this->action('POST', 'AuthController@register', array('username' => 'exampleuser', 'password' => 'pass'));
+        $this->assertEquals("The email field is required.", json_decode($response->getContent())->flash);
+    }
+
+    public function testRegisterLoginRequired()
+    {
+        $response = $this->action('POST', 'AuthController@register', array('email' => 'example@email.com', 'password' => 'pass'));
+        $this->assertEquals("The username field is required.", json_decode($response->getContent())->flash);
+    }
+
+    public function testRegisterPasswordRequired()
+    {
+        $response = $this->action('POST', 'AuthController@register', array('email' => 'example@email.com', 'username' => 'exampleuser'));
+        $this->assertEquals("The password field is required.", json_decode($response->getContent())->flash);
+    }
+
+    public function testRegisterEmailAlreadyTaken()
+    {
+        $response = $this->action('POST', 'AuthController@register', array('email' => 'test@example.com', 'username' => 'exampleuser', 'password' => 'pass'));
+        $this->assertEquals("The email has already been taken.", json_decode($response->getContent())->flash);
+    }
+
+    public function testRegisterUsernameAlreadyTaken()
+    {
+        $response = $this->action('POST', 'AuthController@register', array('email' => 'example@email.com', 'username' => 'test', 'password' => 'pass')); 
+        $this->assertEquals("The username has already been taken.", json_decode($response->getContent())->flash);
+    }
+
+    public function testRegisterOk()
+    {
+        $response = $this->action('POST', 'AuthController@register', array('email' => 'example@email.com', 'username' => 'example', 'password' => 'pass'));
+        $this->assertResponseOk();
     }
 }
 ?>
--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -13,7 +13,22 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		$testEnvironment = 'testing';
 
+
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
+
+        public static $preparedDatabase = false;
+
+        public function setUp()
+        {
+            parent::setUp();
+            if(self::$preparedDatabase == false)
+            {
+                Artisan::call('migrate:refresh');
+                $this->seed();
+                self::$preparedDatabase = true;
+            }
+            Mail::pretend(true);
+        }
 
 }
