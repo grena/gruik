381201b00c4e3c09c22ed7393511d5263620a4c5
Adrien Pétremann <petremann.adrien@gmail.com>
2015-12-04 22:16:23 +0100

    Create Note entity

---
+++ src/AppBundle/Entity/Note.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\UserInterface;
+
+class Note
+{
+    /** @var string */
+    protected $id;
+
+    /** @var string */
+    protected $title;
+
+    /** @var string */
+    protected $content;
+
+    /** @var UserInterface */
+    protected $author;
+
+    /** @var \DateTime */
+    protected $created;
+
+    /** @var \DateTime */
+    protected $updated;
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return UserInterface
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param UserInterface $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param \DateTime $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    /**
+     * @param \DateTime $updated
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    }
+}
