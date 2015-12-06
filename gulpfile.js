39e87ca7f619f62ac98369f823ef97737cbdefa1
Adrien Pétremann <petremann.adrien@gmail.com>
2015-12-06 15:25:05 +0100

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ var gulp = require('gulp');
 
 // Installation task
 gulp.task('install', function(){
-    gulp.src('./node_modules/jquery/dist/jquery.js')
-        .pipe(gulp.dest('./web/vendor/dist/'));
+    gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/ace-builds/src-min/ace.js',
+        './node_modules/ace-builds/src-min/mode-markdown.js',
+        './node_modules/ace-builds/src-min/theme-github.js',
+        './node_modules/marked/lib/marked.js'
+    ]).pipe(gulp.dest('./web/vendor/dist/'));
 });
