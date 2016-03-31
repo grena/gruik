2c8cd7c814fc38d8dc4f72aba077cf9d7a292290
Adrien Pétremann <petremann.adrien@gmail.com>
2016-03-31 23:17:49 +0200

    style(phpdoc): update phpdoc author & license for AppBundle

--- src/Gruik/AppBundle/AppBundle.php
+++ src/Gruik/AppBundle/AppBundle.php
@@ -4,6 +4,10 @@ namespace Gruik\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * @author  grena <hello@grena.fr>
+ * @license https://opensource.org/licenses/GPL-3.0  GNU General Public License v3.0 (GPL-3.0)
+ */
 class AppBundle extends Bundle
 {
 }
--- src/Gruik/AppBundle/Controller/DefaultController.php
+++ src/Gruik/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @author  grena <hello@grena.fr>
+ * @license https://opensource.org/licenses/GPL-3.0  GNU General Public License v3.0 (GPL-3.0)
+ */
 class DefaultController extends Controller
 {
     /**
