a84d05936c4cb1bea1f6e0e78dd983138337fba8
Adrien Pétremann <petremann.adrien@gmail.com>
2016-04-02 23:41:32 +0200

    refactor(appKernel): register FOS & HWI bundle for oAuth features

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
             new Gruik\AppBundle\AppBundle(),
         );
 
