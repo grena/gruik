2b9c364684e6bdc1e6b00e2e450c6226913e369c
Adrien Pétremann <petremann.adrien@gmail.com>
2016-04-03 01:05:58 +0200

    design(general): replace bootstrap by semantic ui

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,15 @@ var concat = require('gulp-concat');
 gulp.task('install', function() {
     gulp.src([
         './node_modules/jquery/dist/jquery.min.js',
-        './node_modules/bootstrap/dist/js/bootstrap.min.js',
-        './node_modules/bootstrap/dist/css/bootstrap.min.css',
         './node_modules/font-awesome/css/font-awesome.min.css'
     ]).pipe(gulp.dest('./web/vendor/dist/'));
 
     gulp.src(
         './node_modules/font-awesome/fonts/**/*' , {base: './node_modules/font-awesome/'}
+    ).pipe(gulp.dest('./web/vendor/'));
+
+    gulp.src(
+        './node_modules/semantic-ui-css/**/*', {base: './node_modules/'}
     ).pipe(gulp.dest('./web/vendor/'));
 });
 
